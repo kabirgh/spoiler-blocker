e75118c9e8ba00198edecbe00e4906d7db395426
kabir-plod <kabirk@live.com>
2016-05-31 16:47:35 +0800

    using mutation-summary

--- firefox/data/fb.js
+++ firefox/data/fb.js
@@ -1,32 +1,21 @@
 $(document).ready(function () {
 	console.log("START");
 
-	var spoilersArr = ["the", "a"];
-	var streamId = "substream_0";
 	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+	var spoilersArr = ["the", "a"];
 
-	var insertedNodes = [];
-	var observer = new MutationObserver(function(mutations) {
-	 mutations.forEach(function(mutation) {
-	   for (var i = 0; i < mutation.addedNodes.length; i++)
-	     insertedNodes.push(mutation.addedNodes[i]);
-			 console.log("Hi");
-			 console.log(insertedNodes);
-	 })
-	});
-	observer.observe(document, { subtree: true, characterData: true });
+	// // Find new streams
+	// var feedObserver = new MutationObserver( function(mutations) {
+	// 	console.log("observing");
+	// 	console.log(mutations.type);
 
-	// Find new streams
-		console.log("observing");
-		console.log(mutations.type);
-
-		mutations.forEach(function(mutation) {
-			$(mutation.addedNodes)
-			.each( function() {
-				console.log("node ID " + $(this).attr("id"));
-			});
-		});
-	});
+	// 	mutations.forEach(function(mutation) {
+	// 		$(mutation.addedNodes)
+	// 		.each( function() {
+	// 			console.log("node ID " + $(this).attr("id"));
+	// 		});
+	// 	});
+	// });
 
 	// Check for feed_stream's existence
 	document.addEventListener("DOMNodeInserted", findFeed);
@@ -44,15 +33,29 @@ $(document).ready(function () {
 		else {
 			console.log("target ID is " + $(feed[0]).attr("id"));
 
-			// Observe children of feed
-			feedObserver.observe(feed[0], {
-				childList:true,
-				subtree:true
+			var feedObserver = new MutationSummary({
+				callback: feedLogger,
+				queries: [{
+					element: "div"
+				}]
 			});
+
+			// // Observe children of feed
+			// feedObserver.observe(feed[0], {
+			// 	childList:true,
+			// 	subtree:true
+			// });
 
 			document.removeEventListener("DOMNodeInserted", findFeed);
 			console.log("event removed");
 		}
+	}
+
+
+	function feedLogger(summaries) {
+		summaries[0].added.forEach( function(elem) {
+			console.log($(elem).attr("id"));
+		})
 	}
 
 
@@ -65,9 +68,9 @@ $(document).ready(function () {
 		// which contain any of the spoilers in spoilersArr.
 		// Give these elements the "long-string-..." attribute.
 		var posts = $("div[id^='" + parentId + "']").find("div[id^='hyperfeed_story']")
-						.attr("story", "yesitiszzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
-						.has( containsAny("p", spoilersArr) )
-						.attr("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+		.attr("story", "yesitiszzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+		.has( containsAny("p", spoilersArr) )
+		.attr("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
 	}
 
 
