8dbd0848be540459b8b0878421e07dc924b2c417
varung97 <varung97@yahoo.co.in>
2016-06-02 18:04:35 +0800

    facebook spoiler overlay works

--- chrome/test.js
+++ chrome/test.js
@@ -70,7 +70,61 @@ jQuery(document).ready( function($) {
 	function hidePosts(summaries) {
 		elem = $(summaries[0].added).filter("[class^='userContentWrapper']");
 		if (elem.length > 0) {
-			console.log(elem.text());
+			postText = elem.text();
+			toHide = false;
+			for (var i=0; i<spoilersArr.length; i++) {
+				// if tweet text contains a spoiler
+				if (postText.indexOf( spoilersArr[i] ) > -1) {
+					// tweetNode should be hidden
+					toHide = true;
+				}
+			}
+
+			if (toHide) {
+				elem = $(elem[0]);
+				console.log(elem.text());
+
+				newDiv = $(document.createElement("div")).css({
+					'position': 'absolute',
+					'top': 0,
+					'left': 0,
+					'background-color': 'white',
+					'width': '100%',
+					'height': '99%',
+					'z-index': 7,
+					'cursor': 'pointer'
+				});
+
+				lineHeight = elem.height() * 0.9;
+
+				// Spoiler text
+				newDiv.append($('<p/>').text('Spoiler!').css({
+					'position': 'absolute',
+					'top': 0,
+					'left': 0,
+					'background-color': 'white',
+					'width': '100%',
+					'height': '100%',
+					'font-size': 40,
+					'text-align': 'center',
+					'line-height': lineHeight.toString() + 'px',
+					'font-family': 'Copperplate',
+					'color': 'red',
+					'margin': '0px'
+				}));
+
+				// Absolutely positioned element needs a positioned ancestor
+				// This does not break any of twitter's formatting (far as I have seen)
+				elem.css({
+					'position': 'relative'
+				})
+
+				newDiv.click(function() {
+					$(this).hide()
+				});
+
+				elem.append(newDiv);
+			}
 		}
 	}
 
