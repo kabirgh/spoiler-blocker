0c9f6b387023e627fb9e1e54f0a78eb5ffd394ff
kabir-plod <kabirk@live.com>
2016-09-25 12:28:32 +0800

    added twitter copy logic

--- copy_files.py
+++ copy_files.py
@@ -1,3 +1,4 @@
+###################### FIREFOX #####################
 # fb-ff
 # fb-common
 # utils-common
@@ -17,6 +18,25 @@ open('./firefox/data/fb-exp.js', 'w+') as fb_exp:
 	fb_exp.truncate()
 
 
+# tw-ff
+# tw-common
+# utils-common
+with open('./firefox/data/tw-ff.js', 'r') as tw_ff, \
+open('tw-common.js', 'r') as tw_common, \
+open('utils-common.js', 'r') as utils_common, \
+open('./firefox/data/tw-exp.js', 'w+') as tw_exp:
+	for line in tw_ff:
+		tw_exp.write(line)
+
+	for line in tw_common:
+		tw_exp.write(line)
+
+	for line in utils_common:
+		tw_exp.write(line)
+
+	tw_exp.truncate()
+
+###################### CHROME #####################
 # fb-chrome
 # fb-common
 # utils-common
@@ -34,3 +54,22 @@ open('./chrome/fb-exp.js', 'w+') as fb_exp:
 		fb_exp.write(line)
 
 	fb_exp.truncate()
+
+
+# tw-chrome
+# tw-common
+# utils-common
+with open('./chrome/tw-chr.js', 'r') as tw_chrome, \
+open('tw-common.js', 'r') as tw_common, \
+open('utils-common.js', 'r') as utils_common, \
+open('./chrome/tw-exp.js', 'w+') as tw_exp:
+	for line in tw_chrome:
+		tw_exp.write(line)
+
+	for line in tw_common:
+		tw_exp.write(line)
+
+	for line in utils_common:
+		tw_exp.write(line)
+
+	tw_exp.truncate()
