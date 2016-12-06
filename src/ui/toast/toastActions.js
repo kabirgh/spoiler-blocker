94b112823c07819cc5dcb4d7102445020776a6e8
kabir-plod <kabir-plod@users.noreply.github.com>
2016-12-06 21:23:33 +0530

    Opening options menu increases popup height to fit (#4)
    * removed TODO abt syncing to storage
    
    * inlined options menu element, resizes popup
    
    * fixed bug with wrong toast showing
    
    * created production build

---
+++ src/ui/toast/toastActions.js
@@ -0,0 +1,13 @@
+import {action} from "mobx";
+import ToastStore from "./ToastStore";
+
+module.exports = {
+	resetToastObject: action(resetToastObject)
+};
+
+function resetToastObject() {
+	ToastStore.isAddSuccess = false;
+	ToastStore.isInvalidTitleOrTags = false;
+	ToastStore.isDuplicateTitle = false;
+	ToastStore.isListDeleted = false;
+}
