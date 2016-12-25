77015982d7e1b9735e3903c799a28713f834d9ae
varung97 <varung97@yahoo.co.in>
2016-12-25 10:45:27 +0800

    Extract editTags and editTitle into editList

--- src/panel/spoiler_card/spoilerCardActions.js
+++ src/panel/spoiler_card/spoilerCardActions.js
@@ -21,23 +21,21 @@ function isValidTitle(index, title) {
 
 function editTitle(index, title, tags) {
 	console.log("editTitle called");
-
-	MainStore.spoilers[index]["title"] = title.trim();
-	MainStore.spoilers[index]["tags"] = commonActions.tagStringToArray(tags);
-
-	console.log("new title on next line");
-	console.log(title);
-
-	console.log("spoiler tags for 1st list on next line");
-	console.log(MainStore.spoilers[0]["tags"]);
+	editList(index, title, tags);
 }
 
 // TODO: disallow duplicate tags?
 function editTags(index, title, tags) {
 	console.log("editTags called");
+	editList(index, title, tags);
+}
 
-	MainStore.spoilers[index]["title"] = title;
+function editList(index, title, tags) {
+	MainStore.spoilers[index]["title"] = title.trim();
 	MainStore.spoilers[index]["tags"] = commonActions.tagStringToArray(tags);
+
+	console.log("new title on next line");
+	console.log(title);
 
 	console.log("spoiler tags for 1st list on next line");
 	console.log(MainStore.spoilers[0]["tags"]);
