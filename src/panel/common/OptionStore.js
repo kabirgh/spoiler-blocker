ec264020dd54b0e16ee3c1b892a0d8627bf873a3
kabir-plod <kabir-plod@users.noreply.github.com>
2017-01-10 12:44:07 +0530

    Update npm dependencies (#31)
    * updated non-breaking dependencies
    
    * fixed npm run start

--- src/panel/common/OptionStore.js
+++ src/panel/common/OptionStore.js
@@ -9,26 +9,29 @@ class Store {
 
 const OptionStore = new Store();
 
-chrome.storage.local.get("prefs", function(obj) {
-	console.log("prefs get: " + JSON.stringify(obj["prefs"]));
+if (process.env.NODE_ENV !== "devServer") {
+	chrome.storage.local.get("prefs", function(obj) {
+		console.log("prefs get: " + JSON.stringify(obj["prefs"]));
 
-	if (obj["prefs"] === undefined || obj["prefs"] === {}) {
-		console.log("if block");
-		OptionStore.prefs = {
-			defaultHidePref: "overlay", // or remove
-			defaultCaseSensitivity: false
-		};
-	}
-	else {
-		OptionStore.prefs = obj["prefs"];
-	}
-});
+		if (obj["prefs"] === undefined || obj["prefs"] === {}) {
+			console.log("if block");
+			OptionStore.prefs = {
+				defaultHidePref: "overlay", // or remove
+				defaultCaseSensitivity: false
+			};
+		}
+		else {
+			OptionStore.prefs = obj["prefs"];
+		}
+	});
+	
 
-autorun(() => {
-	// Accesses all properties recursively, triggering autorun when any change is made
-	JSON.stringify(OptionStore.prefs);
-	console.log("autorun optionstore set prefs: " + JSON.stringify(OptionStore.prefs));
-	chrome.storage.local.set({"prefs": toJS(OptionStore.prefs)});
-});
+	autorun(() => {
+		// Accesses all properties recursively, triggering autorun when any change is made
+		JSON.stringify(OptionStore.prefs);
+		console.log("autorun optionstore set prefs: " + JSON.stringify(OptionStore.prefs));
+		chrome.storage.local.set({"prefs": toJS(OptionStore.prefs)});
+	});
+}
 
 export default OptionStore;
